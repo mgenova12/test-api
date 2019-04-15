fb4ab6101a673c232b7680a557e092bdfa81dd2c
Marco <mgenova03@gmail.com>
2019-04-15 13:51:52 -0400

    add cor config

--- config/application.rb
+++ config/application.rb
@@ -25,14 +25,8 @@ module TestApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'http://localhost:3000', 'https://desolate-waters-78828.herokuapp.com/' #replace this url with that of your own heroku client app
-        resource '*', :headers => :any, :methods => [:get]
-      end
-    end
 
-    
+
     config.api_only = true
   end
 end
